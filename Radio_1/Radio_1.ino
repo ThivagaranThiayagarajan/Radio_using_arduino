a6b809045d38134c80ff3c5c623b2d7cf4cbcd0d
thivagaran.thiayagarajan@gmail.com <thivagaran.thiayagarajan@gmail.com>
2021-06-25 07:30:09 +0530

    created file and added library

---
+++ Radio_1/Radio_1.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <radio.h>
+#include <RDA5807M.h>
+
+#define FIX_BAND     RADIO_BAND_FM   ///< The band that will be tuned by this sketch is FM.
+#define FIX_STATION  9110           ///< The station that will be tuned by this sketch is 89.30 MHz.
+#define FIX_VOLUME   10               ///< The volume that will be set by this sketch is level 4.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
